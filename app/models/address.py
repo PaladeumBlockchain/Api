bc688520e7ab79925f6e341031894ca2392cf30c
Kuyugama <mail.kuyugama@gmail.com>
2025-10-18 22:36:31 +0300

    Add address locked balance

--- app/models/address.py
+++ app/models/address.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import mapped_column, relationship
-from sqlalchemy.orm import Mapped
 from sqlalchemy import Numeric, ForeignKey
+from sqlalchemy.orm import Mapped
 from sqlalchemy import String
+from decimal import Decimal
 from .base import Base
 
 
@@ -10,19 +11,16 @@ class Address(Base):
 
     address: Mapped[str] = mapped_column(String(70), index=True, unique=True)
 
-    balances: Mapped[list["AddressBalance"]] = relationship(
-        back_populates="address"
-    )
+    balances: Mapped[list["AddressBalance"]] = relationship(back_populates="address")
 
 
 class AddressBalance(Base):
     __tablename__ = "service_address_balances"
-    balance: Mapped[Numeric] = mapped_column(Numeric(28, 8))
+    balance: Mapped[Decimal] = mapped_column(Numeric(28, 8), default=Decimal())
+    locked: Mapped[Decimal] = mapped_column(Numeric(28, 8), default=Decimal())
     currency: Mapped[str] = mapped_column(String(64), index=True)
 
-    address_id = mapped_column(
-        ForeignKey("service_addresses.id"), primary_key=True
-    )
+    address_id = mapped_column(ForeignKey("service_addresses.id"), primary_key=True)
     address: Mapped[Address] = relationship(
         foreign_keys=[address_id], back_populates="balances"
     )
