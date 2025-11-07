6b4376b49a6edc038c653703b3e1c0e54a8cebc4
volbil <volbilnexus@gmail.com>
2025-11-07 11:15:30 +0200

    Bug fix

--- app/general/router.py
+++ app/general/router.py
@@ -8,9 +8,9 @@ TOTAL_SUPPLY = 1000000000
 
 @router.get("/plain/supply/total", response_class=PlainTextResponse)
 async def total_supply():
-    return TOTAL_SUPPLY
+    return str(TOTAL_SUPPLY)
 
 
 @router.get("/plain/supply/circulating", response_class=PlainTextResponse)
 async def circulating_supply():
-    return TOTAL_SUPPLY * 0.5
+    return str(TOTAL_SUPPLY * 0.5)
