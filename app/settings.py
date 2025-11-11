6bd983a799a739cb62b0274c482035f5d2ce271d
Kuyugama <mail.kuyugama@gmail.com>
2025-11-11 18:23:55 +0200

    Add script for transaction fee calculation

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,10 @@
 from functools import lru_cache
 from dynaconf import Dynaconf
+import typing
 
 
 @lru_cache()
-def get_settings():
+def get_settings() -> typing.Any:
     return Dynaconf(
         settings_files=["settings.toml"],
         default_env="default",
